d965d5c304b4b04f58d4dd805909940d8d49aadc
Nikhil Reddy <s.nikhilreddy3446@gamil.com>
2025-11-12 18:56:40 +0530

    fixing

---
+++ android/app/src/main/java/app/lovable/efbe28a2ea794dd8a955a30390665a53/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.efbe28a2ea794dd8a955a30390665a53;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.efbe28a2ea794dd8a955a30390665a53',
   appName: 'calmcanvas-hub',
   webDir: 'dist',
-  server: {
-    url: 'https://efbe28a2-ea79-4dd8-a955-a30390665a53.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   android: {
     allowMixedContent: true
   }
